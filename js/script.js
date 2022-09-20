c30a1fb9a57931d5ab60476451648e44a10b85b8
unknown <albirosaldoalfredo12@gmail.com>
2022-09-20 12:51:12 +0200

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,65 @@
+
+/*
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+  }
+  const nome = document.querySelector('.maquina_de_escever_nome');
+  typeWriter(nome);
+
+*/
+
+
+let navbar = document.querySelector(".nav")
+ let contar = 250
+
+document.addEventListener('scroll', () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > contar) {
+    navbar.classList.add("active")
+   } else {
+    navbar.classList.remove("active")
+  }
+});
+
+
+
+let menuBar = document.querySelector(".links")
+document.querySelector(".fa-bars").onclick = () =>{
+  menuBar.classList.add("active")
+}
+document.querySelector(".fa-xmark").onclick = () =>{
+  menuBar.classList.remove("active")
+}
+window.onscroll = () =>{
+  menuBar.classList.remove("active")
+
+
+}
+
+function loading(){
+  document.querySelector(".processa_box").style.display = "none"
+ var a = document.querySelector(".tudo")
+ a.style.visibility = "visible"
+ window. sr = ScrollReveal({ reset: true });
+//sr.reveal(".imageScale", { duration: 1000, scale: 0.85})
+sr.reveal(".primeiro_flex", { duration: 1000, distance: "150px"})
+sr.reveal(".segundo_flex", { duration: 1000, distance: "100px", delay:"250"})
+sr.reveal(".container_dados_pessoais", { duration: 1000, distance: "150px"})
+sr.reveal(".my_service", { duration: 1000, distance: "150px"})
+
+
+ 
+ /*
+  let aparecer = document.getElementsByTagName("section")
+  for(let i = 0; i < aparecer.length; i++){
+    aparecer[i].style.display = "block"
+  }
+  
+*/
+
+}
+
