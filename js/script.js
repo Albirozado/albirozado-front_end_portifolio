7a1fd9a557c663156696196ea175c9c5a29fcc0a
unknown <albirosaldoalfredo12@gmail.com>
2022-11-07 10:16:26 +0200

    mais um projeto

--- js/script.js
+++ js/script.js
@@ -79,4 +79,11 @@ sr.reveal(".my_service", { duration: 1000, distance: "150px"})
 */
 
 }
-
+function openCity(cityName) {
+  var i;
+  var x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  document.getElementById(cityName).style.display = "grid";  
+}
